dc6c2663f4af1f9bd497a6fcc0869a146f273442
alona <alena.istomina.work@gmail.com>
2025-09-29 07:06:57 +0300

    remove unused types

--- apps/backend/src/types/tables.ts
+++ apps/backend/src/types/tables.ts
@@ -1,8 +1,4 @@
-import { postsTable } from "@/db/schemas/posts";
 import { usersTable } from "@/db/schemas/users";
 
 export type InsertUser = typeof usersTable.$inferInsert;
 export type SelectUser = typeof usersTable.$inferSelect;
-
-export type InsertPost = typeof postsTable.$inferInsert;
-export type SelectPost = typeof postsTable.$inferSelect;
