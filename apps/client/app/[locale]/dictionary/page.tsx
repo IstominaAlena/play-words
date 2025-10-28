602998f08d99c547375f1e56e178b571ec7e202c
Alex <64218691+Alesha0103@users.noreply.github.com>
2025-10-28 14:16:21 +0200

    under construction page (#28)

---
+++ apps/client/app/[locale]/dictionary/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+import { UnderConstructionPage } from "@/components/pages/under-construction-page";
+
+export const metadata: Metadata = {
+    title: "Dictionary",
+    description: "",
+    keywords: "",
+};
+
+const Dictionary = () => <UnderConstructionPage />;
+
+export default Dictionary;
---
+++ apps/client/app/[locale]/games/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+import { UnderConstructionPage } from "@/components/pages/under-construction-page";
+
+export const metadata: Metadata = {
+    title: "Games",
+    description: "",
+    keywords: "",
+};
+
+const Games = () => <UnderConstructionPage />;
+
+export default Games;
